13d5fb4c6b5c148e42c52e00cb257da888598e04
yip <yipkwunleong@gmail.com>
2022-06-04 17:30:27 +0800

    delete git shell

--- git.sh
+++
@@ -1,9 +0,0 @@
-export BRANCH_NAME=$1
-git init
-git remote rm origin
-git remote add origin https://github.com/yip-kl/azure_databricks.git
-git branch $BRANCH_NAME
-git branch -M $BRANCH_NAME
-git add .
-git commit -m "$2"
-git push -u origin $BRANCH_NAME
